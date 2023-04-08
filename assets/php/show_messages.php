8d0c6270315aae4bd975c6051e10a4d4c48c3f60
KeyderDev <ajomar321@gmail.com>
2023-04-08 18:42:34 -0300

    MySql Messages

--- assets/php/show_messages.php
+++ assets/php/show_messages.php
@@ -34,7 +34,9 @@ if ($result->num_rows > 0) {
     if (preg_match('/^\*\*(.*)\*\*$/', $message, $matches)) {
         $message = '<b>' . $matches[1] . '</b>';
     }
-
+    if (preg_match('/^`(.*)`$/', $message, $matches)) {
+        $message = '<span style="background-color: #242526; color: white;">' . $matches[1] . '</span>';
+    }
     echo "<p><strong><img id=\"noimg\" src=\"../assets/images/noimg.png\">" . htmlspecialchars($row["author"]). ":<br></strong><span style=\"color:$color;\"> " . $message . "</span></p>";
   }
 } else {
