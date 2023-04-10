3dbd111ce3099d1e4cb9a7acc403aabe89c24c28
KeyderDev <ajomar321@gmail.com>
2023-04-10 20:18:39 -0300

    Changes

--- content/cdp-general.php
+++ content/cdp-general.php
@@ -12,15 +12,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/switches.css">
-    <link rel="stylesheet" href="../assets/css/image.css">
-    <title>Discogram Beta</title>
-</head>
+<?php include('../assets/php/components/head-content.php');?>
 <style>
     .message-item {
         margin-bottom: 16px;
